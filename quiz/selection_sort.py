27dbe86735ae4cd1925174a4c0ebac5bfd1e358b
Bernardo Fontes <bernardoxhc@gmail.com>
2010-05-25 22:09:25 -0300

    Refatoração do código!

--- quiz/selection_sort.py
+++ quiz/selection_sort.py
@@ -1,16 +1,14 @@
 def selection_sort(sortable_list):
     list_copy = sortable_list[:]
-    for i, number in enumerate(list_copy):
+    for i in xrange(len(list_copy)):
         minor_pos = find_min_position(list_copy[i:])
         minor_pos += i
         list_copy[i], list_copy[minor_pos] = list_copy[minor_pos], list_copy[i]
     return list_copy
 
 def find_min_position(integer_list):
-    minor_integer = integer_list[0]
     minor_position = 0
-    for i, integer in enumerate(integer_list):
-        if integer <= minor_integer:
+    for i in xrange(len(integer_list)):
+        if integer_list[i] <= integer_list[minor_position]:
             minor_position = i
-            minor_integer = integer
     return minor_position
